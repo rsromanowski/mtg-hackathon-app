412e6e70112786ac587ea22fb6e02078f7b31ea6
Richard Romanowski <richard@romanow.ski>
2024-02-29 11:18:04 -0500

    no db connect

--- src/main/kotlin/io/github/rsromanowski/Application.kt
+++ src/main/kotlin/io/github/rsromanowski/Application.kt
@@ -9,19 +9,19 @@ import io.ktor.server.netty.*
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
     configureRouting()
 
-    DatabaseSingleton.createHikariDataSource(
-        environment.config.propertyOrNull("DATABASE_URL")?.getString()
-            ?: error("DATABASE_URL not configured")
-    ).let {
-        doDatabaseThings(it)
-    }
+//    DatabaseSingleton.createHikariDataSource(
+//        environment.config.propertyOrNull("DATABASE_URL")?.getString()
+//            ?: error("DATABASE_URL not configured")
+//    ).let {
+//        doDatabaseThings(it)
+//    }
 
 //    val cardClient: MagicCardClient = ScryfallClient(log)
 //    runBlocking { cardClient.getMkmCards() }
